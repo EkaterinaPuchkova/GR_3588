857f16ca47dbb3bdda4b9688ed4b914eccf955ff
Katerina <modest_girl93@mail.ru>
2022-12-22 12:41:59 +0400

    add solution for task 39,40,41,42,43,44

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,42 @@
+﻿// int Number = ReadData("Введите элементы(через пробел): ");
+// PrintData("Кол-во элементов больше 0: " + CountPosNum(Number));
+
+// //Метод, считывающий данные, введенные пользователем
+// int ReadData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     return int.Parse(Console.ReadLine() ?? "0");
+// }
+
+// //Метод, выводящий на печать данные пользователю
+// void PrintData(string res)
+// {
+//     Console.WriteLine(res);
+// }
+// // Метод, вычесление чисел > 0
+// int CountPosNum( int num)
+// {
+//    int res = 0;
+//     while (num > 0)
+//     {
+//         if(ReadData("Введите элементы(через пробел): ")>0) res++;
+//         num=num-1;
+//     }
+//     return res;
+
+// }
+
+
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
