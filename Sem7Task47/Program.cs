0d101f88c50fe0bcdb528dd38c958b0848c65b76
Katerina <modest_girl93@mail.ru>
2022-12-23 20:28:52 +0400

    add solution for task 46,47,48,49,50,51,52

---
+++ Sem7Task47/Program.cs
@@ -0,0 +1,40 @@
+﻿int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Универсальный метод генерации и заполнение двумерного массива
+void Fill2DArray(double[,] array)
+{
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {   
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+int linesVol = ReadData("Введите количество строк ");
+int columnsVol = ReadData("Введите количество столбцов ");
+double[,] numbers = new double[linesVol, columnsVol];
+Fill2DArray(numbers);
+PrintArray(numbers);
